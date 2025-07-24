78830120dcd444fd65959d0f546fb244022e8aa3
youngmin <skdudals99@gmail.com>
2025-07-24 05:19:37 +0000

    first commit

---
+++ support_files/infra_as_a_code/us-east-1/attach_iam_policy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws iam attach-role-policy --role-name lab-edu-role-vscode --policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess
+aws iam attach-role-policy --role-name lab-edu-role-vscode --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess
+
+echo "The attached role policies in lab-edu-role-vscode: "
+aws iam list-attached-role-policies --role-name lab-edu-role-vscode
+
